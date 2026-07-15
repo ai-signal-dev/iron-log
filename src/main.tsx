1324a7389a7b7b2441853ea30f961f4df0352555
fumiya-0520 <fumiya.t.bn238@gmail.com>
2026-07-15 20:42:09 +0900

    fix: router basename and PWA icon

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/iron-log">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
